aad8ef91f960ed49eba8d91a0470293103db426a
Jason Maher <jason@jasonmaher.me>
2017-02-13 16:10:45 +0900

    process the database table by table

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const DestinyApi = require('destiny-api-client');
 const download = require('./lib/download');
+const processDb = require('./lib/process-db');
 
 let destiny = new DestinyApi(process.env.API_KEY);
 
+let counts = {};
+
+function processor(table, data) {
+    if (!counts[table]) {
+        counts[table] = 0;
+    }
+    counts[table]++;
+}
+
 destiny.manifest()
-    .then(m => {
-        console.log(m);
-        return m;
-    })
-    .then(m => download(m))
-    .then(r => console.log(r));
+    .then(m => download(m, {langs: ['en']}))
+    .then(r => processDb(r[0].dbfile, processor))
+    .then(() => console.log(counts));
